2c9777a9e217c5b45baf66950f2177cf96d5c67a
Julie Crotzer <jcrotzerrn@aol.com>
2022-03-28 15:11:28 -0500

    updated albums.py

--- flask_app/controllers/albums.py
+++ flask_app/controllers/albums.py
@@ -4,5 +4,58 @@ from flask_app.models.user import User
 from flask_app.models.album import Album
 from flask import render_template,redirect,request, session
 
+# CREATE
 
+@app.route("/create/post", methods=['POST'])
+def create_album():
+    if not Album.validate_album(request.form):
+        return redirect('/albums/new')
+    data = {
+        "name": request.form['name'],
+        "artist": request.form['artist'],
+        "release_date": request.form['release_date'],
+        "favorite_tracks": request.form['favorite_tracks'],
+        "user_id": session['user_id']
+    }
+    Album.save(data)
+    return redirect('/dashboard')
 
+# READ
+
+@app.route('/albums/new')
+def add_album():
+    return render_template("create_album.html")
+
+@app.route('/album/<int:id>')
+def display_album(id):
+    this_album = Album.get_one({"id": id})
+    return render_template("dashboard.html", album = this_album)
+
+# UPDATE
+
+@app.route('/albums/edit/<int:id>')
+def edit_album(id):
+    album_to_edit = Album.get_one({"id": id})
+    return render_template("edit_album.html", album = album_to_edit)
+
+@app.route('/edit/post', methods=['POST'])
+def update_album():
+    if not Album.validate_album(request.form):
+        return redirect('/albums/new')
+    data = {
+        "id": request.form['id'],
+        "name": request.form['name'],
+        "artist": request.form['artist'],
+        "release_date": request.form['release_date'],
+        "favorite_tracks": request.form['favorite_tracks'],
+        "user_id": session['user_id']
+    }
+    Album.update(data)
+    return redirect('/dashboard')
+
+# DELETE
+
+@app.route('/albums/delete/<int:id>')
+def delete_album(id):
+    Album.destroy({"id": id})
+    return redirect('/dashboard')
