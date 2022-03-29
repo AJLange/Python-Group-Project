ac5a991979b9502aa865593df5ac2bed152fd684
Sue Yang <sueyang112@hotmail.com>
2022-03-29 19:26:19 -0400

    Added the user_id on the Album Class

--- flask_app/models/album.py
+++ flask_app/models/album.py
@@ -2,33 +2,37 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app import app
 from flask_app.models.user import User
 from flask import flash
+
+
 class Album:
     db = ("fav_albums")
+
     def __init__(self, data):
         self.id = data['id']
         self.name = data['name']
         self.artist = data['artist']
-        self.release_date= data['release_date']
+        self.release_date = data['release_date']
         self.favorite_tracks = data['favorite_tracks']
+        self.user_id = data['user_id']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         self.liked_by = []
         self.posted_by = []
 
     @classmethod
-    def save(cls,data):
-        query = "INSERT INTO albums (name,artist,release_date,favorite_tracks) VALUES (%(name)s,%(artist)s,%(release_date)s,%(favorite_tracks)s);"
-        return connectToMySQL(cls.db).query_db(query,data)
+    def save(cls, data):
+        query = "INSERT INTO albums (name,artist,release_date,favorite_tracks,user_id) VALUES (%(name)s,%(artist)s,%(release_date)s,%(favorite_tracks)s,%(user_id)s);"
+        return connectToMySQL(cls.db).query_db(query, data)
 
     @classmethod
-    def destroy(cls,data):
+    def destroy(cls, data):
         query = "DELETE FROM albums WHERE albums.id = %(id)s;"
-        return connectToMySQL(cls.db).query_db(query,data)
+        return connectToMySQL(cls.db).query_db(query, data)
 
     @classmethod
     def update(cls, data):
         query = "UPDATE albums SET name=%(name)s, artist=%(artist)s, release_date=%(release_date)s, favorite_tracks=%(favorite_tracks)s, date_made=%(date_made)s,updated_at=NOW() WHERE id = %(id)s;"
-        return connectToMySQL(cls.db).query_db(query,data)
+        return connectToMySQL(cls.db).query_db(query, data)
 
     @classmethod
     def get_all(cls):
@@ -37,7 +41,7 @@ class Album:
         all_albums = []
         for row in results:
             user_data = {
-                "id" : row["users.id"],
+                "id": row["users.id"],
                 "first_name": row["first_name"],
                 "last_name": row["last_name"],
                 "email": row['email'],
@@ -47,7 +51,7 @@ class Album:
             }
             this_user = User(user_data)
             row["user"] = this_user
-            all_albums.append( cls(row) )
+            all_albums.append(cls(row))
         return all_albums
 
     @classmethod
@@ -67,8 +71,8 @@ class Album:
         print(results)
         this_album = cls(album_data)
         for row in results:
-                user_data = {
-                "id" : row["users.id"],
+            user_data = {
+                "id": row["users.id"],
                 "first_name": row["first_name"],
                 "last_name": row["last_name"],
                 "email": row['email'],
@@ -80,9 +84,9 @@ class Album:
         return this_album
 
     @classmethod
-    def get_liked_albums(cls,data):
+    def get_liked_albums(cls, data):
         query = "SELECT * from albums WHERE albums.id IN (SELECT album_id FROM likes WHERE user_id = %(id)s );"
-        results =connectToMySQL(cls.db).query_db(query,data)
+        results = connectToMySQL(cls.db).query_db(query, data)
         albums = []
         for a in results:
             albums.append(cls(a))
@@ -98,28 +102,28 @@ class Album:
         return albums
 
     @classmethod
-    def like_album(cls,data):
-            query = "INSERT INTO likes (user_id,album_id) VALUES (%(user_id)s,%(album_id)s);"
-            return connectToMySQL(cls.db).query_db(query,data)
+    def like_album(cls, data):
+        query = "INSERT INTO likes (user_id,album_id) VALUES (%(user_id)s,%(album_id)s);"
+        return connectToMySQL(cls.db).query_db(query, data)
 
     @classmethod
-    def unlike_album(cls,data):
+    def unlike_album(cls, data):
         query = "DELETE FROM likes WHERE user_id = %(user_id)s and album_id = %(album_id)s;"
-        return connectToMySQL(cls.db).query_db(query,data)
+        return connectToMySQL(cls.db).query_db(query, data)
 
     @staticmethod
     def validate_album(album):
         is_valid = True
         if len(album['name']) < 3:
             is_valid = False
-            flash("Name must be at least 3 characters","album")
+            flash("Name must be at least 3 characters", "album")
         if len(album['artist']) < 3:
             is_valid = False
-            flash("Artist names' must be at least 3 characters","album")
+            flash("Artist names' must be at least 3 characters", "album")
         if album['release_date'] == "":
             is_valid = False
-            flash("Please enter a date","album")
+            flash("Please enter a date", "album")
         if len(album['favorite_tracks']) < 3:
             is_valid = False
-            flash("Favorite tracks must be at least 3 characters","album")
+            flash("Favorite tracks must be at least 3 characters", "album")
         return is_valid
